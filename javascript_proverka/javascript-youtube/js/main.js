d02656b061dffd5f527688f625c00342cbe77941
NikSargs <nik.bishkek00@gmail.com>
2023-02-05 22:43:55 +0600

    JS youtube-vebkademi - objects

--- javascript_proverka/javascript-youtube/js/main.js
+++ javascript_proverka/javascript-youtube/js/main.js
@@ -1,14 +1,62 @@
 
 
-const person = {
-	userName : 'Jo',
-	age : 30,
-	isMarried : false
+// const person = {
+// 	userName : 'Jo',
+// 	age : 30,
+// 	isMarried : false,
+// 	sayHi: function (name) {
+// 		console.log(this);
+// 		console.log( `Hi ${name}! My name is ${this.userName}` );
+// 	}
+
+
+// };
+
+// person.sayHi("Mark");
+
+// const person = {
+// 	userName : 'Jo',
+// 	age : 30,
+// 	isMarried : false,
+// 	sayHi: function (name) {
+// 		console.log(this);
+// 		console.log( `Hi ${name}! My name is ${this.userName}` );
+// 	}
+// };
+
+
+// for (let key in person) {
+// 	console.log(key, ':', person[key]);
+// }
+
+// const person1 = {
+// 	userName : 'Jo',
+// 	age : 30,
+// 	isMarried : false,
+// 	sayHi: function (name) {
+// 		console.log(this);
+// 		console.log( `Hi ${name}! My name is ${this.userName}` );
+// 	}
+// };
+
+
+
+class Person {
+	constructor(userName, age, isMarried) {
+		this.userName = userName;
+		this.age = age;
+		this.isMarried = isMarried;
+	}
+	sayHi(name) {
+		console.log(`Hi ${name}! My name is ${this.userName}`);
+	}
 }
 
-console.log(person);
 
-console.log(person.userName);
+const person1 = new Person('Jo', 30, false);
+const person2 = new Person('Bob', 28, true);
 
-person.profession = 'JS developer';
-console.log(person);
+console.log(person1);
+console.log(person2);
+
+person2.sayHi('Lee');
