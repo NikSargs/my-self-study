81200e27c3b074d6fd95957c476f198eea97c064
nikitak <nikita.kucheryavyi@ctechnology.kg>
2023-08-17 17:32:35 +0600

    update 17 08 23

---
+++ oop-lesson-15/src/com/dmdev/Lesson19/ArrayListRunner.java
@@ -0,0 +1,19 @@
+package com.dmdev.Lesson19;
+
+
+
+import java.util.ArrayList;
+
+public class ArrayListRunner {
+    public static void main(String[] args) {
+        ArrayList<Integer> integers = new ArrayList<>();
+        integers.add(1);
+        integers.add(2);
+        integers.add(3);
+        integers.add(4);
+        integers.add(5);
+        integers.add(6);
+
+        System.out.println();
+    }
+}
--- oop-lesson-15/src/com/dmdev/lesson18/WeaponRunner.java
+++ oop-lesson-15/src/com/dmdev/lesson18/WeaponRunner.java
@@ -1,9 +1,12 @@
 package com.dmdev.lesson18;
 
 import com.dmdev.lesson15.Archer;
+import com.dmdev.lesson15.Hero;
 import com.dmdev.lesson15.Warrior;
 import com.dmdev.lesson18.weapon.Bow;
+import com.dmdev.lesson18.weapon.MeleeWeapon;
 import com.dmdev.lesson18.weapon.Sword;
+import com.dmdev.lesson18.weapon.Weapon;
 
 public class WeaponRunner {
     public static void main(String[] args) {
@@ -12,5 +15,11 @@ public class WeaponRunner {
 
         Warrior<Sword> warrior = new Warrior<>("Боромир", 10);
         warrior.setWeapon(new Sword());
+
+        printWeaponDamage(archer);
+    }
+
+    public static <T extends Weapon> void printWeaponDamage(Hero<T> hero) {
+        System.out.println(hero.getWeapon().getDamage());
     }
 }
