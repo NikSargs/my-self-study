413e366c6af2880b4ddb52cfd7997c4eeb2733ad
nikitak <nikita.kucheryavyi@ctechnology.kg>
2023-08-24 17:17:49 +0600

    update 24 08 23

---
+++ java/src/OddOrEven/OddOrEvenCheck.java
@@ -0,0 +1,7 @@
+package OddOrEven;
+
+public class OddOrEvenCheck {
+    public static void main(String[] args) {
+        System.out.println(OddOrEven.find(new int[] {206847684, 1056521, 7, 17, 1901, 21104421, 7, 1, 35521, 1, 7781}));
+    }
+}
